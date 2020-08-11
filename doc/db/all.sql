018229fa84995833dae75971459620f3c2abecf8
a510334890 <510334890@qq.com>
2020-08-11 16:32:42 +0800

    新建大章表并且生成实体类和sql语句

--- doc/db/all.sql
+++ doc/db/all.sql
@@ -1,2 +1,11 @@
 insert into test (id, name) values ('3','wangwu');
 select * from test;
+
+-- 大章
+drop table if exists `chapter`;
+create table `chapter` (
+  `id` char(8) not null comment 'id',
+  `course_id` char(8) comment '课程id',
+  `name` varchar(50) comment '名称',
+  primary key (`id`)
+) engine=innodb default charset=utf8mb4 comment='大章';
