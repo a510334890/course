e8a5603e3a6dfe779311f7cb50e38c3d967f4e51
a510334890 <510334890@qq.com>
2020-09-19 15:42:32 +0800

    集成分页插件

--- server/src/main/java/com/course/server/service/ChapterService.java
+++ server/src/main/java/com/course/server/service/ChapterService.java
@@ -4,6 +4,7 @@ import com.course.server.domain.Chapter;
 import com.course.server.domain.ChapterExample;
 import com.course.server.dto.ChapterDto;
 import com.course.server.mapper.ChapterMapper;
+import com.github.pagehelper.PageHelper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -15,9 +16,10 @@ public class ChapterService {
     @Resource
     private ChapterMapper chapterMapper;
     public List<ChapterDto> list(){
+        PageHelper.startPage(1,10);//设置分页从1开始，不是0,根据最近的查询语句
         ChapterExample chapterExample = new ChapterExample();
         chapterExample.createCriteria().andCourseIdEqualTo("00000000");
-        chapterExample.setOrderByClause("id desc");//根据id进行排序查询
+        chapterExample.setOrderByClause("id asc");//根据id进行排序查询
         /*new TestExample().setOrderByClause("id");*/
         List<Chapter> chapters = chapterMapper.selectByExample(chapterExample);
         List<ChapterDto> chapterDtos = new ArrayList<>();
