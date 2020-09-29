c62e01c033a6a3bd61ec7ab43a5ee8f55aae0598
a510334890 <510334890@qq.com>
2020-09-29 11:42:51 +0800

    增加controller自动生成模块

--- generator/src/main/java/com/course/generator/server/ServerGenerator.java
+++ generator/src/main/java/com/course/generator/server/ServerGenerator.java
@@ -7,14 +7,20 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ServerGenerator {
+    static String MODULE="business";
     static String toServicePath = "server//src//main//java//com//course//server//service//";
-    static String toControllerPath = "business//src//main//java//com//course//business//controller//admin//";
+    static String toControllerPath = MODULE+"business//src//main//java//com//course//"+MODULE+"//controller//admin//";
+
     public static void main(String[] args) throws Exception {
         String Domain = "Section";
         String domain = "section";
+        String tablieNameCn="小节";
+        String module = MODULE;
         Map<String,Object> map = new HashMap<>();
         map.put("Domain",Domain);
         map.put("domain",domain);
+        map.put("tablieNameCn",tablieNameCn);
+        map.put("module",module);
         FreemarkerUtil.intiConfig("service.ftl");
         FreemarkerUtil.generator(toServicePath+Domain+"Service.java",map);
         FreemarkerUtil.intiConfig("controller.ftl");
